bb1229294e9f1ecd176831c492cd4d30857b2b89
Ahlam Mamoun <mestar.ale@gmail.com>
2022-11-10 19:44:42 +0100

    design pattern mvc

---
+++ public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+
+require __DIR__ . '/../app/Controllers/MainController.php';
+
+
+if (isset($_GET['_url'])) {
+    $pageToDisplay = $_GET['_url'];
+} else {
+    $pageToDisplay = '/';
+}
+
+$routes = [
+
+
+    '/' => 'homeAction',
+];
+//var_dump($routes);
+if (isset($routes[$pageToDisplay])) {
+
+
+    $controller = new MainController();
+
+
+    $methodToCall = $routes[$pageToDisplay];
+
+
+    $controller->$methodToCall();
+
+
+} else {
+    $controller = new MainController();
+    $controller->pageNotFound();
+}
